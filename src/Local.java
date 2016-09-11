3db27c5bf9fa349d67557364c248493b9ad0b276
VeterAlex <twinzata@gmail.com>
2016-09-11 23:11:34 +0300

    Initial commit

---
+++ src/Local.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Veter on 11.09.2016.
+ */
+public class Local {
+    public static void main(String[] args) {
+        int a = 56;
+        int b = 76;
+        int sum = a + b;
+
+        System.out.println(sum);
+    }
+}
